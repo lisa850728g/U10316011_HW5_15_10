6d03e683045aaf54f708de6eb82dea0923fd262c
lisa850728g <lisa850728g@gmail.com>
2015-04-26 13:58:01 +0800

    create MyStack class
    MyStack class in listing 11.10

---
+++ MyStack.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class MyStack{
+	
+  public static void main(String[] args) {
+	
+    ArrayList<Object> list = new ArrayList<Object>();
+	
+    public boolean isEmpty(){
+      return list.isEmpty();
+    }
+	
+    public int getSize(){
+      return list.getSize();
+    }
+	
+    public Object peek(){
+      return  list.get(getSize()-1);
+    }
+
+    public Object pop(){
+      Object o = list.get(getSize()-1);
+      list.remove(getSize()-1);
+      return 0;
+    }
+	
+    public void push(Object o){
+      list.add(o);
+    }
+	
+    @Override
+    public String toString(){
+      return "stack: " + list.toString();
+    }
+}
