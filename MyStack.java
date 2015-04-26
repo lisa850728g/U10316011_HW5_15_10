57fe602352374bebb61e258bf1d2a104a4bb8b57
lisa850728g <lisa850728g@gmail.com>
2015-04-26 14:04:01 +0800

    change MyStack class
    將MyStack實作至Cloneable
    加入clone method 型態為Object
    由於Object類別哩，平台實作的clone method會進行物件複製的動作，
    所以method裡面會簡單呼叫super.clone()，
    然而定義於Object類別的clone method will throw CloneNotSupportedException
    因此在header須加上CloneNotSupportedException

--- MyStack.java
+++ MyStack.java
@@ -1,35 +1,39 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class MyStack{
-	
-  public static void main(String[] args) {
-	
-    ArrayList<Object> list = new ArrayList<Object>();
-	
-    public boolean isEmpty(){
-      return list.isEmpty();
-    }
-	
-    public int getSize(){
-      return list.getSize();
-    }
-	
-    public Object peek(){
-      return  list.get(getSize()-1);
-    }
+class MyStack implements Cloneable{
+	public ArrayList<Object> list = new ArrayList<Object>();
+		
+	public boolean isEmpty(){
+		return list.isEmpty();
+	}
+		
+	public int getSize(){
+		return list.size();
+	}
+		
+	public Object peek(){
+		return  list.get(getSize()-1);
+	}
 
-    public Object pop(){
-      Object o = list.get(getSize()-1);
-      list.remove(getSize()-1);
-      return 0;
-    }
+	public Object pop(){
+		Object o = list.get(getSize()-1);
+		list.remove(getSize()-1);
+		return o;
+	}
+		
+	public void push(Object o){
+		list.add(o);
+	}
 	
-    public void push(Object o){
-      list.add(o);
-    }
-	
-    @Override
-    public String toString(){
-      return "stack: " + list.toString();
-    }
+	@SuppressWarnings("unchecked")
+	public  Object clone() throws CloneNotSupportedException{
+		MyStack stackClone = (MyStack)super.clone();
+		stackClone.list = (ArrayList<Object>)(list.clone());
+		return stackClone;
+	}
+		
+	@Override
+	public String toString(){
+		return "stack: " + list.toString();
+	}
 }
